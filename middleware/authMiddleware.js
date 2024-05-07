7d071dd67aebf41aa1490ac9d2db565685a481f7
Dmytro Horbatov <117715290+MettelBach@users.noreply.github.com>
2024-05-07 18:25:25 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,27 +19,6 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-const requireUnAuth = (req, res, next) => {
-    try {
-    const token = req.cookies.jwt;
-
-    if (token) {
-        jwt.verify(token, "mettel bach secret", (error, decodedToken) => {
-            if (error) {
-                console.log(error.message);
-                res.redirect("/signin");
-            } else {
-                console.log(decodedToken);
-                res.redirect("/profile");
-                next();
-            }
-        });
-    }
-    } catch (error) {
-        console.log("ok");
-    }
-}
-
 const checkUser = (req, res, next) => {
     const token = req.cookies.jwt;
 
@@ -62,4 +41,4 @@ const checkUser = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth, checkUser, requireUnAuth }
+module.exports = { requireAuth, checkUser }
